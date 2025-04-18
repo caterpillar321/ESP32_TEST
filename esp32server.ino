f7f590d3d18548eaa26a67d9a40143df520efdb0
caterpillar321 <kbktw0705@gmail.com>
2025-04-18 19:37:09 +0900

    Update esp32server.ino

--- esp32server.ino
+++ esp32server.ino
@@ -3,13 +3,13 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+//#include <Adafruit_GFX.h>
+//#include <Adafruit_SSD1306.h>
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+//Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 // Android에서 사용할 UUID (예시 값, Android에서도 똑같이 사용해야 함)
 #define SERVICE_UUID        "12345678-1234-5678-1234-56789abcdef0"
@@ -42,17 +42,17 @@ class MyCallbacks : public BLECharacteristicCallbacks {
     if (value.length() > 0) {
       Serial.print("Received Data :  ");
       Serial.println(value);
-
-      display.clearDisplay();
-      display.setCursor(0, 0);
-      display.setTextSize(1);         // 글자 크기 (1 배율)
-      display.setTextColor(WHITE);    // 글자 색 (흰색)
-      display.println(value);         // 받은 문자열 출력
-      display.display();    
+      //수신된 데이터의 처리 및 작동이 여기서 정의가 되야 합니다.
+      //display.clearDisplay();
+      //display.setCursor(0, 0);
+      //display.setTextSize(1);         // 글자 크기 (1 배율)
+      //display.setTextColor(WHITE);    // 글자 색 (흰색)
+      //display.println(value);         // 받은 문자열 출력
+      //display.display();    
 
       // 받은 데이터를 그대로 다시 보내기 (에코 응답)
-      pCharacteristic->setValue(value);
-      pCharacteristic->notify(); // Android로 전송
+      //pCharacteristic->setValue(value);
+      //pCharacteristic->notify(); // Android로 전송
     }
   }
 };
@@ -62,18 +62,18 @@ void setup() {
 
   // SSD1306 디스플레이 제어를 위한 코드로, 실제 장치의 경우 필요 없을겁니다.
   // I2C 초기화 (ESP32 기본 SDA: GPIO21, SCL: GPIO22)
-  Wire.begin(21, 22);
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); 
-  }
-  // 화면 제어 초기화 코드입니다. 실제 장치의 경우 필요 없을 겁니다.
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
-  display.println("Init done!");
-  display.display();
+  //Wire.begin(21, 22);
+  //if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+  //  Serial.println(F("SSD1306 allocation failed"));
+  //  for(;;); 
+  //}
+  // 화면 제어 초기화 코드입니다. 실제// 장치의 경우 필요 없을 겁니다.
+  //display.clearDisplay();
+  //display.setTextSize(1);
+  //display.setTextColor(WHITE);
+  //display.setCursor(0, 0);
+  //display.println("Init done!");
+  //display.display();
 
 
   BLEDevice::init("ESP32");
